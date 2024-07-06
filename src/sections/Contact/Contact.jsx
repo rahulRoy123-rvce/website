c96f8d853d7596d8d44dad0d8ba449875bd5e650
karthikeya-kotegar <karthikeyakotegar@gmail.com>
2024-07-06 15:08:04 +0530

    email

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+    emailjs.sendForm('service_zuseae6', 'template_h2u7s9h', form.current, 'h-B19s-FImd5w300r')
       .then((result) => {
           console.log(result.text);
           alert('Message sent successfully!');
@@ -20,37 +20,31 @@ function Contact() {
   return (
     <section id="contact" className="flex flex-col items-stretch text-center">
       <h1 className="text-2xl mb-4">Contact</h1>
-      <form ref={form} onSubmit={sendEmail} className="w-full max-w-lg">
+      <form ref={form} onSubmit={sendEmail} className="w-full max-w-lg mx-auto">
         <div className="mb-4">
-          <label htmlFor="name" className="sr-only">
-            Name
-          </label>
+          <label htmlFor="from_name" className="sr-only">Name</label>
           <input
             type="text"
-            name="name"
-            id="name"
+            name="from_name"
+            id="from_name"
             placeholder="Name"
             required
             className="w-full p-2 mb-4 border border-gray-300 rounded"
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="email" className="sr-only">
-            Email
-          </label>
+          <label htmlFor="from_email" className="sr-only">Email</label>
           <input
             type="email"
-            name="email"
-            id="email"
+            name="from_email"
+            id="from_email"
             placeholder="Email"
             required
             className="w-full p-2 mb-4 border border-gray-300 rounded"
           />
         </div>
         <div className="mb-4">
-          <label htmlFor="message" className="sr-only">
-            Message
-          </label>
+          <label htmlFor="message" className="sr-only">Message</label>
           <textarea
             name="message"
             id="message"
